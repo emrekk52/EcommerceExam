f579b5ce924e39a44c82ac74927745a08e1d21e1
emre kucuk <emre.kucuk@logo.com.tr>
2022-03-23 12:31:10 +0300

    update product list

--- app/src/main/java/com/ecommerce/exam/utils/Extensions.kt
+++ app/src/main/java/com/ecommerce/exam/utils/Extensions.kt
@@ -14,16 +14,6 @@ import com.ecommerce.exam.R
 
 fun Activity.fullScreen(isDark: Boolean = false) {
 
-    if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
-    }
-
-    if (Build.VERSION.SDK_INT >= 19) {
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-    }
-
-
 
     if (!isDark) {
         window.decorView.setSystemUiVisibility(window.decorView.getSystemUiVisibility() and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv())
@@ -33,11 +23,8 @@ fun Activity.fullScreen(isDark: Boolean = false) {
 
 
 
-
-    if (Build.VERSION.SDK_INT >= 21) {
-        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
-        window.statusBarColor = Color.TRANSPARENT
-    }
+    setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+    window.statusBarColor = Color.TRANSPARENT
 
 
 }
